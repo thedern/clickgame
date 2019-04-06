02f24bf357eefa3694859a4905fcfa139b13115d
thedern <dxsmith@soverin.net>
2019-04-06 14:43:50 -0400

    Updates

---
+++ precache-manifest.ecae827dd3082c5f88e83dc2bff5d965.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d8fe0c4b196c89ca9ad",
+    "url": "%20//thedern.github.io/clickgame/static/js/runtime~main.6b6c16e2.js"
+  },
+  {
+    "revision": "bf6672c1019aecf8a893",
+    "url": "%20//thedern.github.io/clickgame/static/js/main.252b18ee.chunk.js"
+  },
+  {
+    "revision": "0a5d3c68b562219f5f16",
+    "url": "%20//thedern.github.io/clickgame/static/js/2.38d62158.chunk.js"
+  },
+  {
+    "revision": "bf6672c1019aecf8a893",
+    "url": "%20//thedern.github.io/clickgame/static/css/main.8bc59e43.chunk.css"
+  },
+  {
+    "revision": "0a5d3c68b562219f5f16",
+    "url": "%20//thedern.github.io/clickgame/static/css/2.7134aaf9.chunk.css"
+  },
+  {
+    "revision": "1bc0f8d7fe33e9d3f6f2d028ba542797",
+    "url": "%20//thedern.github.io/clickgame/index.html"
+  }
+];
