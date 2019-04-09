0201e04c8cbbaee31d82573d2c269ca6dbd1d5be
thedern <dxsmith@soverin.net>
2019-04-09 11:04:57 -0400

    updated score

--- src/components/Score.js
+++ src/components/Score.js
@@ -2,18 +2,20 @@ import React, { Component } from 'react';
 
 class Score extends Component {
   render() {
+    const { score } = this.props;
     return (
       <div>
-        <ul className="navbar-nav mr-auto">
+        <ul className="navbar-nav mr-auto mb-3">
           <li className="nav-item text-danger pt-3  scores-text">
-            Current Score:
+            Current Score: {score}
           </li>
         </ul>
+        {/*}
         <ul className="navbar-nav mr-auto">
           <li className="nav-item text-primary pb-3 scores-text">
             Total Score:
           </li>
-        </ul>
+        </ul> */}
       </div>
     );
   }
